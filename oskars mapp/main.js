92b69d139bdbeb90d10ace4ebcbb27f2bc63bac3
Oskar <oskar9899@gmail.com>
2017-10-23 10:43:34 +0200

    Add files via upload

--- oskars mapp/main.js
+++ oskars mapp/main.js
@@ -1,13 +1,15 @@
 function heart() {
-  document.getElementById("rightH").style.cssText="top: calc(35% - 150px);";
-  document.getElementById("leftH").style.cssText="left: calc(50% - 150px);";
+  document.getElementById("rightH").style.cssText="top: -38px; left: 38px";
+  document.getElementById("midH").style.cssText="top: 0px; left: 0px";
+  document.getElementById("leftH").style.cssText="top: -38px; left: -38px;";
 }
+
 function Scroll() {
-  var top = document.getElementById('nav');
+  var top = document.getElementById('button');
   var ypos = window.pageYOffset;
 
   if (ypos > 540) {
-    top.style.cssText="display: inherit;";
+    top.style.cssText="display: inherit; border-bottom: 1px solid black";
   }
   else {
     top.style.cssText ="display: none;";
