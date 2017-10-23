5909ce289dce57bb6b48f566acdb2084b8766585
Harselan <haeressel@gmail.com>
2017-10-23 10:58:01 +0200

    Inkluderade oskars uppdateringar till slutliga koden

--- views/_footer.php
+++ views/_footer.php
@@ -1,3 +1,12 @@
-    <script src="assets/js/main.js"></script>
-    </body>
+<div class="footer">
+  <ul>
+    <li><a href="">Random</a></li>
+    <li><a href="">Random</a></li>
+    <li><a href="">Random</a></li>
+    <li><a href="">Random</a></li>
+  </ul>
+</div>
+
+<script src="assets/js/main.js"></script>
+</body>
 </html>
--- views/dashboard/main.php
+++ views/dashboard/main.php
@@ -1,40 +1,32 @@
-    <a href="/logout">Logga ut</a>
-    <div class="bg"></div>
-    <div class="bg-2"></div>
+<div class="bg"></div>
 
-    <div id="leftH"> < </div>
-    <div id="rightH"> 3 </div>
-
-    <div id="nav">
-      <ul>
-        <li><a href="#">Random</a></li>
-        <li><a href="#">Random</a></li>
-        <li><a href="#">Random</a></li>
-        <li><a href="#">Random</a></li>
-      </ul>
-    </div>
-
-    <div class="buttons">
-      <div class="one"></div>
-      <div class="two"></div>
-      <div class="three"></div>
-      <div class="four"></div>
-    </div>
-
-    <h1>Skriv ett fint meddelande</h1>
-    <p>#NÄTKÄRLEK</p>
+<div class="heart">
+  <div id="leftH"></div>
+  <div id="midH"></div>
+  <div id="rightH"></div>
+</div>
 
 
-    <form class="kommentar" action="/message/add" method="post">
-      <textarea name="text" rows="15" cols="60"></textarea><br>
-      <input type="submit" name="" value="Sprid Kärleken">
-    </form>
+<div id="nav">
+  <ul>
+    <li><a href="#">Random</a></li>
+    <li><a href="#">Random</a></li>
+    <li><a href="#">Random</a></li>
+    <li><a href="#">Random</a></li>
+  </ul>
+</div>
 
-    <div class="footer">
-      <ul>
-        <li><a href="">Random</a></li>
-        <li><a href="">Random</a></li>
-        <li><a href="">Random</a></li>
-        <li><a href="">Random</a></li>
-      </ul>
-    </div>
+<div class="buttons">
+  <div class="one"></div>
+  <div class="two"></div>
+  <a href="/logout"><div class="three"><img src="assets/images/logout.png" alt="logout"></div></a>
+  <div class="four"></div>
+</div>
+
+<h1>Skriv ett fint meddelande</h1>
+<p>#NÄTKÄRLEK</p>
+
+<form class="kommentar" action="/message/add" method="post">
+  <textarea name="text" rows="15" cols="60"></textarea><br>
+  <input type="submit" name="" value="Sprid Kärleken">
+</form>
