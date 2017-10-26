389f596fd1ec9ab711c87c6a98300d3cf6a470d9
Harselan <haeressel@gmail.com>
2017-10-26 14:43:20 +0200

    Fixade så att hjärtats slag är oändliga

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,42 @@
+var a = document.getElementById("rightH");
+var b = document.getElementById("midH");
+var c = document.getElementById("leftH");
+var d = document.getElementById("heart");
 function heart() {
-  document.getElementById("rightH").style.cssText="top: -38px; left: 38px";
-  document.getElementById("midH").style.cssText="top: 0px; left: 0px";
-  document.getElementById("leftH").style.cssText="top: -38px; left: -38px;";
+    a, b, c = true;
+    d = false;
+
+  if (a, b, c == true) {
+    document.getElementById("rightH").style.cssText="top: -38px; left: 38px";
+    document.getElementById("midH").style.cssText="top: 0px; left: 0px";
+    document.getElementById("leftH").style.cssText="top: -38px; left: -38px;";
+    a, b, c = false;
+  }
+}
+
+setTimeout(timeoutBig, 2800);
+
+function timeoutBig() {
+  if (a, b, c, d == false) {
+    document.getElementById("heart").style.cssText="left: calc(50% - 75px);";
+    document.getElementById("rightH").style.cssText="top: -48px; left: 48px; width: 150px; height: 150px; transition: all 1000ms ease-in-out;";
+    document.getElementById("midH").style.cssText="top: 0px; left: 0px; width: 150px; height: 150px; transition: all 1000ms ease-in-out;";
+    document.getElementById("leftH").style.cssText="top: -48px; left: -48px; width: 150px; height: 150px; transition: all 1000ms ease-in-out;";
+    a, b, c, d = true;
+  }
+  setTimeout(timeoutSmall, 1000);
+
+}
+function timeoutSmall() {
+  if (a, b, c, d == true) {
+    document.getElementById("heart").style.cssText="left: calc(50% - 50px);";
+    document.getElementById("rightH").style.cssText="top: -38px; left: 38px; width: 100px; height: 100px; transition: all 1000ms ease-in-out;";
+    document.getElementById("midH").style.cssText="top: 0px; left: 0px; width: 100px; height: 100px; transition: all 1000ms ease-in-out;";
+    document.getElementById("leftH").style.cssText="top: -38px; left: -38px; width: 100px; height: 100px; transition: all 1000ms ease-in-out;";
+    a, b, c, d = false;
+
+  }
+  setTimeout(timeoutBig, 1400);
 }
 // function Scroll() {
 //   var top = document.getElementById('');
