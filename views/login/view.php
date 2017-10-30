ccb5acb9468c9728470af05ff7baabe9c96b455e
Harselan <haeressel@gmail.com>
2017-10-30 17:20:20 +0100

    Tog bort en onödig mapp

--- views/login/view.php
+++
@@ -1,8 +0,0 @@
-<div class="login-wrapper">
-    <h2>Logga in</h2>
-    <form action="/" method="post">
-        <input type="text" name="name" placeholder="Namn:">
-        <input type="password" name="password" placeholder="Lösenord:">
-        <input type="submit" value="Logga in">
-    </form>
-</div>
